6d615da1e343613bde97c1907cf0d21529d050dd
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 19:06:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT a.name, s.name 
+  "SELECT a.name, s.name
   FROM authors a
   LEFT JOIN series s2 ON a.id = s2.author_id
   LEFT JOIN subgenres s ON s.id = s2.subgenre_id;"
