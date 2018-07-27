f236859d8d26c9e5b2a94e690bcfadcde6fb7596
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 19:03:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,9 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, count(species) 
-  FROM characters 
-  GROUP BY species 
+  "SELECT species, count(species)
+  FROM characters
+  GROUP BY species
   ORDER BY count(species) DESC LIMIT 1;"
 end
 
